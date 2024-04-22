5cef1b63fecabd6d3a5bcc9a018cb4c5f0169a1d
Hayronsan <hayronsan@gmail.com>
2024-04-22 16:44:46 +0500

    Eifell tower

--- SwiftUIDemo/Screens/HomeScreen.swift
+++ SwiftUIDemo/Screens/HomeScreen.swift
@@ -9,15 +9,22 @@ import SwiftUI
 
 
 struct HomeScreen: View {
-    
     @State private var half = false
     @State private var dim = false
     
     var body: some View {
         
         VStack {
-            LottieView(animationFileName: "facebook", loopMode: .loop)
-                .frame(width: 200, height: 200)
+            Image("enfil")
+                .resizable()
+                .scaleEffect(half ? 0.5 : 1.0)
+                .opacity(half ? 0.2 : 1.0)
+                .animation(.easeInOut(duration: 1.0))
+                .onTapGesture {
+                    self.dim.toggle()
+                    self.half.toggle()
+                }
+            
                 
         }
         
