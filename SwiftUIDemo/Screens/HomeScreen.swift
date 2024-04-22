e7f9c99ed64ad5ca25258bd2521157fe59af44e4
Hayronsan <hayronsan@gmail.com>
2024-04-22 12:36:40 +0500

    tavView

--- SwiftUIDemo/Screens/HomeScreen.swift
+++ SwiftUIDemo/Screens/HomeScreen.swift
@@ -10,60 +10,26 @@ import SwiftUI
 
 struct HomeScreen: View {
     
-    @State var showingAlert = false
-    @State var isSheet = false
-    var actioSHeet: ActionSheet {
-        let title = "IOS"
-        let message = "Here we go"
-        return ActionSheet(title: Text(title), message: Text(message), buttons: [
-            .default(Text("OK"),action: {
-                
-            }),
-            .destructive(Text("NO"), action: {
-                
-            })
-        
-        ])
-    }
     
     var body: some View {
         
         TabView {
             
-            Button(action: {
-                showingAlert = true
-            }, label: {
-                Text("Alert Dialog")
-                    .font(.title)
-            }).alert(isPresented: $showingAlert, content: {
-                let title = "IOS"
-                let message = "Here we go"
-                return Alert(title: Text(title), message: Text(message), primaryButton: .destructive(Text("NO")){
-                    
-                }, secondaryButton: .default(Text("OK")){
-                    
-                })
-            })
-                .tabItem {
-                    Image(systemName: "circle")
-                    Text("Alert")
-                }
-            
-            Button(action: {
-                isSheet = true
-            }, label: {
-                Text("Action Sheet")
-                    .font(.title)
-            }).actionSheet(isPresented: $isSheet, content: {
-                self.actioSHeet
-            })
-                .tabItem {
-                    Image(systemName: "circle")
-                    Text("Sheet")
-                }
-            
+            Image("photo1")
+                .resizable()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            Image("photo2")
+                .resizable()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            Image("photo3")
+                .resizable()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            Image("photo4")
+                .resizable()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
             
         }
+        .tabViewStyle(PageTabViewStyle())
         
         
     }
