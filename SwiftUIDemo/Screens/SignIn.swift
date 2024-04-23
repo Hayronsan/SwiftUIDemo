1ef03152647d9f6742eb1afe0316d721f699e5ce
Hayronsan <hayronsan@gmail.com>
2024-04-23 15:28:26 +0500

    SIgnIn and SignUp

---
+++ SwiftUIDemo/Screens/SignIn.swift
@@ -0,0 +1,18 @@
+//
+//  SignIn.swift
+//  SwiftUIDemo
+//
+//  Created by Nodir on 23/04/24.
+//
+
+import SwiftUI
+
+struct SignIn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignIn()
+}
---
+++ SwiftUIDemo/Screens/SignUp.swift
@@ -0,0 +1,18 @@
+//
+//  SignUp.swift
+//  SwiftUIDemo
+//
+//  Created by Nodir on 23/04/24.
+//
+
+import SwiftUI
+
+struct SignUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUp()
+}
