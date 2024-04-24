b8ac8b0ff07122809a9f97bafa7803534c29577b
Hayronsan <hayronsan@gmail.com>
2024-04-24 18:59:41 +0500

    Observable Object

---
+++ SwiftUIDemo/Classes/Status.swift
@@ -0,0 +1,8 @@
+//
+//  Status.swift
+//  SwiftUIDemo
+//
+//  Created by Nodir on 24/04/24.
+//
+
+import Foundation
---
+++ SwiftUIDemo/Screens/Starter.swift
@@ -0,0 +1,18 @@
+//
+//  Starter.swift
+//  SwiftUIDemo
+//
+//  Created by Nodir on 24/04/24.
+//
+
+import SwiftUI
+
+struct Starter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Starter()
+}
